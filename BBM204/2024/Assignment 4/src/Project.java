01648d046fd1add2db43e688826f7eb964138e93
a-emreusta <a.emreusta@hotmail.com>
2024-05-05 16:33:24 +0300

    part 1 done!

--- BBM204/2024/Assignment 4/src/Project.java
+++ BBM204/2024/Assignment 4/src/Project.java
@@ -15,12 +15,22 @@ public class Project implements Serializable {
      * @return the total duration of the project in days
      */
     public int getProjectDuration() {
+        if (tasks.isEmpty()) return 0; // If no tasks, duration is zero.
+    
+        // Assume getEarliestSchedule method has been correctly implemented
+        int[] startTimes = getEarliestSchedule();
         int projectDuration = 0;
-
-        // TODO: YOUR CODE HERE
-
+    
+        for (int i = 0; i < tasks.size(); i++) {
+            int taskEnd = startTimes[i] + tasks.get(i).getDuration();
+            if (taskEnd > projectDuration) {
+                projectDuration = taskEnd;
+            }
+        }
+    
         return projectDuration;
     }
+    
 
     /**
      * Schedule all tasks within this project such that they will be completed as early as possible.
@@ -28,11 +38,52 @@ public class Project implements Serializable {
      * @return An integer array consisting of the earliest start days for each task.
      */
     public int[] getEarliestSchedule() {
-
-        // TODO: YOUR CODE HERE
-
-        return null;
+        int numTasks = tasks.size();
+        int[] earliestStarts = new int[numTasks];
+        int[] indegree = new int[numTasks];
+        List<List<Integer>> adjList = new ArrayList<>();
+    
+        // Initialize adjacency list
+        for (int i = 0; i < numTasks; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    
+        // Build graph and compute indegrees
+        for (int i = 0; i < numTasks; i++) {
+            Task currentTask = tasks.get(i);
+            for (Integer dep : currentTask.getDependencies()) {
+                adjList.get(dep).add(i);
+                indegree[i]++;
+            }
+        }
+    
+        // Queue for tasks that are ready to be processed
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numTasks; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+                earliestStarts[i] = 0; // Start as soon as possible
+            }
+        }
+    
+        // Process tasks in topological order
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int neighbor : adjList.get(current)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+                int possibleStart = earliestStarts[current] + tasks.get(current).getDuration();
+                if (possibleStart > earliestStarts[neighbor]) {
+                    earliestStarts[neighbor] = possibleStart;
+                }
+            }
+        }
+    
+        return earliestStarts;
     }
+    
 
     public static void printlnDash(int limit, char symbol) {
         for (int i = 0; i < limit; i++) System.out.print(symbol);
--- BBM204/2024/Assignment 4/src/UrbanInfrastructureDevelopment.java
+++ BBM204/2024/Assignment 4/src/UrbanInfrastructureDevelopment.java
@@ -1,5 +1,12 @@
+import java.io.File;
 import java.io.Serializable;
 import java.util.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class UrbanInfrastructureDevelopment implements Serializable {
     static final long serialVersionUID = 88L;
@@ -10,8 +17,12 @@ public class UrbanInfrastructureDevelopment implements Serializable {
      * @param projectList a list of Project objects
      */
     public void printSchedule(List<Project> projectList) {
-        // TODO: YOUR CODE HERE
+        for (Project project : projectList) {
+            int[] schedule = project.getEarliestSchedule();
+            project.printSchedule(schedule);
+        }
     }
+    
 
     /**
      * TODO: Parse the input XML file and return a list of Project objects
@@ -20,8 +31,46 @@ public class UrbanInfrastructureDevelopment implements Serializable {
      * @return a list of Project objects
      */
     public List<Project> readXML(String filename) {
-        List<Project> projectList = new ArrayList<>();
-        // TODO: YOUR CODE HERE
-        return projectList;
+    List<Project> projectList = new ArrayList<>();
+    try {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(new File(filename));
+        doc.getDocumentElement().normalize();
+
+        NodeList nodeList = doc.getElementsByTagName("Project");
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                String projectName = element.getElementsByTagName("Name").item(0).getTextContent();
+                List<Task> tasks = new ArrayList<>();
+
+                NodeList tasksList = element.getElementsByTagName("Task");
+                for (int j = 0; j < tasksList.getLength(); j++) {
+                    Node taskNode = tasksList.item(j);
+                    if (taskNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element taskElement = (Element) taskNode;
+                        int taskID = Integer.parseInt(taskElement.getElementsByTagName("TaskID").item(0).getTextContent());
+                        String description = taskElement.getElementsByTagName("Description").item(0).getTextContent();
+                        int duration = Integer.parseInt(taskElement.getElementsByTagName("Duration").item(0).getTextContent());
+                        List<Integer> dependencies = new ArrayList<>();
+
+                        NodeList dependenciesList = taskElement.getElementsByTagName("DependsOnTaskID");
+                        for (int k = 0; k < dependenciesList.getLength(); k++) {
+                            dependencies.add(Integer.parseInt(dependenciesList.item(k).getTextContent()));
+                        }
+                        tasks.add(new Task(taskID, description, duration, dependencies));
+                    }
+                }
+                projectList.add(new Project(projectName, tasks));
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+    return projectList;
+}
 }
