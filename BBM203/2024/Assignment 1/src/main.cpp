879d0071662490895c6feb27db0ca2c34e029949
a-emreusta <a.emreusta@hotmail.com>
2024-11-18 16:39:33 +0300

    initial commit

--- BBM203/2024/Assignment 1/src/main.cpp
+++ BBM203/2024/Assignment 1/src/main.cpp
@@ -19,7 +19,6 @@ void apply_mean_filter(const char* input_image, int kernel_size) {
     Filter::apply_mean_filter(img, kernel_size);
     std::string output_filename = "mean_filtered_" + remove_extension(input_image) + "_" + std::to_string(kernel_size) + ".png";
     img.save_to_file(output_filename.c_str());
-}
 
 // Applies Gaussian smoothing to the input image and saves the result
 void apply_gaussian_smoothing(const char* input_image, int kernel_size, double sigma) {
